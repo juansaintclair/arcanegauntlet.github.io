a2490c583b896ad7df401409586ada9ab8e78b13
juansaintclair <juan.pimentel@g.globo>
2025-07-03 17:18:23 -0300

    assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: 'assets',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'assets/monsters', dest: 'assets' },
+        { src: 'assets/player', dest: 'assets' },
+      ],
+    }),
+  ],
+  publicDir: false,
   server: {
     // This is needed to make Vercel dev work with API routes
     proxy: {
